ed5a2212ce13ff914f99033e78612cd9e51e3dc1
MrDoel <l0c4l_r00t@yahoo.co.id>
2016-05-18 13:27:35 +0700

    Update wp-user-enum.py

--- wp-user-enum.py
+++ wp-user-enum.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-#WP User Enum
-#Author     : Mr.Doel
-#E-email    : doel@indonesiancoder.com
+"""
+WP User Enum
+Author     : Mr.Doel
+E-email    : doel@indonesiancoder.com
+"""
 import urllib2,sys
 url=sys.argv[1]
 for num in range(15):
@@ -10,4 +12,4 @@ for num in range(15):
         geturi=str(response.geturl())
         print geturi.split("/author",1)[1].replace("/",'').lstrip()
     except Exception, e:
-        pass
+    
